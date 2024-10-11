bdac5da2281d82033a3d93fc599ab0bbafbb2d04
¨CarlosHa44¨ <carlosha176@gmail.com>
2024-10-11 01:02:58 -0500

    PSet1

---
+++ week1/CHernandez/Psets/Pset1/uwu.js
@@ -0,0 +1,61 @@
+
+function setPlaceholder() {
+    const input = document.getElementById('quantity');
+    input.placeholder = "6.69"; // Cambia el placeholder al enfocar
+}
+
+function resetPlaceholder() {
+    const input = document.getElementById('quantity');
+    if (input.value === "") {
+        input.placeholder = " "; // Vuelve a un espacio si el input está vacío
+    }
+}
+
+// Lógica del algoritmo
+const getQuantity = (quantityRaw) => {
+    return Math.round(quantityRaw * 100);
+};
+
+const calculateCoins = (quantity) => {
+    const coins = [25, 10, 5, 1];
+    let numberOfCoins = [0, 0, 0, 0];
+
+    for (let i = 0; i < 4; i++) {
+        while (quantity >= coins[i]) {
+            numberOfCoins[i]++;
+            quantity = quantity - coins[i];
+        }
+    }
+
+    return numberOfCoins;
+};
+
+const mainFunction = (event) => {
+    event.preventDefault();
+
+    const value = parseFloat(document.getElementById('quantity').value);
+    const valueNoRaw = getQuantity(value);
+
+    let numberOfCoins = calculateCoins(valueNoRaw);
+
+    const quarters = numberOfCoins[0];
+    const dimes = numberOfCoins[1];
+    const nickels = numberOfCoins[2];
+    const pennies = numberOfCoins[3];
+    const total=quarters+dimes+nickels+pennies;
+
+    const content = document.getElementById('calcs');
+    content.innerHTML = `
+        <div class="p-6 bg-slate-800 rounded-lg shadow-lg text-center mt-6">
+            <h2 class="text-2xl font-bold text-white mb-4">Resultados</h2>
+            <div class="text-lg text-blue-400 mb-2">Quarters: <span class="text-blue-300 font-semibold">${quarters}</span></div>
+            <div class="text-lg text-green-400 mb-2">Dimes: <span class="text-green-300 font-semibold">${dimes}</span></div>
+            <div class="text-lg text-yellow-400 mb-2">Nickels: <span class="text-yellow-300 font-semibold">${nickels}</span></div>
+            <div class="text-lg text-pink-400 mb-2">Pennies: <span class="text-pink-300 font-semibold">${pennies}</span></div>
+            <div class="text-3xl text-blue-400 mb-2">Total Coins: <span class="text-blue-300 font-semibold">${total}</span></div>
+        </div>
+    `;
+};
+
+document.getElementById('info').addEventListener('submit', mainFunction);
+
