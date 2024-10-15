e8ae9a9d382dd9576ac178b0909275c02ee365e2
Olivier Paspuel <olivier_paspuel@hotmail.com>
2024-10-14 19:39:09 -0500

    CW02 - Diaz: I really liked the design of the page, I would recommend using a CSS framework. Clear JS logic, few lines of code, great use. Great job. 5/5

--- week1/SDiaz/Classwork/js/main.js
+++ week1/SDiaz/Classwork/js/main.js
@@ -1,12 +1,14 @@
 const form = document.getElementById('iotForm');
 const messageSection = document.getElementById('message');
 
+// callback can be an arrow function 
 form.addEventListener('submit', function(event){
     event.preventDefault();
 
     const name = document.getElementById('firstName').value;
     const favoriteIoT = document.getElementById('IoT').value;
 
+    // great use of fstrings
     const message = `Hello ${name}, your favorite IoT device is ${favoriteIoT}.`;
 
     messageSection.textContent = message;
